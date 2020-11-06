3ef2e67f6bbfecf31928d60b2b1ebad158340fa7
momo3159 <tommydsha@gmail.com>
2020-11-06 22:39:27 +0900

    Create A.cpp

---
+++ ABC114/A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+using ll = long long;
+
+int main() {
+    int X;
+    cin >> X;
+
+    if(X == 3 || X == 5 || X == 7) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
